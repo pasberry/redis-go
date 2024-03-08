fecc7928e4d38944395fa7c0e2075a2f51542fdc
Phillip Asberry <phillip.j.asberry@gmail.com>
2024-03-07 23:24:09 -0500

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -1,23 +1,56 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net"
 	"os"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		log.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	//ensure the teardown of the server on application termination.
+	defer listener.Close()
+	log.Println("The server is up and listening on port 6379")
+
+	for {
+		//Block until we recieve an incoming request
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Error:", err)
+			continue
+		}
+
+		//handler method.
+		handleClient(conn)
+
 	}
+}
+
+func handleClient(conn net.Conn) {
+
+	//make sure we close the listener
+	defer conn.Close()
+
+	//read the data
+	buf := make([]byte, 1024)
+
+	input, err := conn.Read(buf)
+	if err != nil {
+		log.Println("Error", err)
+	}
+
+	log.Println("Received Data:", buf[:input])
+
+	//write the same data back to the user.
+	_, err = conn.Write(buf[:input])
+	if err != nil {
+		log.Println("Error:", err)
+	}
+
 }
